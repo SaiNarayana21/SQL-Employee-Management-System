4c5781ae49841de8c93d69bf167bdc5a6d748c8d
SaiNarayana21 <145527529+SaiNarayana21@users.noreply.github.com>
2025-12-10 15:05:23 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,2 @@
+# SQL_Employee-Management-System
+Employee Management System using MySQL – A relational database project to manage employees, departments, salaries, leaves, qualifications, and payroll. Features payroll automation, leave tracking, and data insights with SQL queries.
---
+++ SQL_PROJECT.sql
@@ -0,0 +1,241 @@
+CREATE DATABASE EMPLOYEE_MANAGEMENT_SYSTEM;
+USE EMPLOYEE_MANAGEMENT_SYSTEM;
+
+-- Table 1: Job Department
+CREATE TABLE JobDepartment (
+    job_ID INT PRIMARY KEY,
+    jobdept VARCHAR(50),
+    name VARCHAR(100),
+    description TEXT,
+    salary_Range VARCHAR(50)
+);
+SELECT * FROM JOBDEPARTMENT;
+
+-- Table 2: Salary/Bonus
+CREATE TABLE SalaryBonus (
+    salary_ID INT PRIMARY KEY,
+    job_ID INT,
+    amount DECIMAL(10,2),
+    annual DECIMAL(10,2),
+    bonus DECIMAL(10,2),
+    CONSTRAINT fk_salary_job FOREIGN KEY (job_ID) REFERENCES JobDepartment(job_ID)
+        ON DELETE CASCADE ON UPDATE CASCADE
+);
+SELECT * FROM SalaryBonus;
+
+-- Table 3: Employee
+CREATE TABLE Employee (
+    emp_ID INT PRIMARY KEY,
+    firstname VARCHAR(50),
+    lastname VARCHAR(50),
+    gender VARCHAR(10),
+    age INT,
+    contact_add VARCHAR(100),
+    emp_email VARCHAR(100) UNIQUE,
+    emp_pass VARCHAR(50),
+    Job_ID INT,
+    CONSTRAINT fk_employee_job FOREIGN KEY (Job_ID)
+        REFERENCES JobDepartment(Job_ID)
+        ON DELETE SET NULL
+        ON UPDATE CASCADE
+);
+SELECT * FROM EMPLOYEE;
+
+-- Table 4: Qualification
+CREATE TABLE Qualification (
+    QualID INT PRIMARY KEY,
+    Emp_ID INT,
+    Position VARCHAR(50),
+    Requirements VARCHAR(255),
+    Date_In DATE,
+    CONSTRAINT fk_qualification_emp FOREIGN KEY (Emp_ID)
+        REFERENCES Employee(emp_ID)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE
+);
+SELECT * FROM QUALIFICATION;
+
+-- Table 5: Leaves
+CREATE TABLE Leaves (
+    leave_ID INT PRIMARY KEY,
+    emp_ID INT,
+    date DATE,
+    reason TEXT,
+    CONSTRAINT fk_leave_emp FOREIGN KEY (emp_ID) REFERENCES Employee(emp_ID)
+        ON DELETE CASCADE ON UPDATE CASCADE
+);
+SELECT * FROM LEAVES;
+
+-- Table 6: Payroll
+CREATE TABLE Payroll (
+    payroll_ID INT PRIMARY KEY,
+    emp_ID INT,
+    job_ID INT,
+    salary_ID INT,
+    leave_ID INT,
+    date DATE,
+    report TEXT,
+    total_amount DECIMAL(10,2),
+    CONSTRAINT fk_payroll_emp FOREIGN KEY (emp_ID) REFERENCES Employee(emp_ID)
+        ON DELETE CASCADE ON UPDATE CASCADE,
+    CONSTRAINT fk_payroll_job FOREIGN KEY (job_ID) REFERENCES JobDepartment(job_ID)
+        ON DELETE CASCADE ON UPDATE CASCADE,
+    CONSTRAINT fk_payroll_salary FOREIGN KEY (salary_ID) REFERENCES SalaryBonus(salary_ID)
+        ON DELETE CASCADE ON UPDATE CASCADE,
+    CONSTRAINT fk_payroll_leave FOREIGN KEY (leave_ID) REFERENCES Leaves(leave_ID)
+        ON DELETE SET NULL ON UPDATE CASCADE
+);
+SELECT * FROM PAYROLL;
+
+-- 1. EMPLOYEE INSIGHTS
+-- How many unique employees are currently in the system?
+SELECT DISTINCT COUNT(*) AS UNIQUE_EMPLOYEES FROM EMPLOYEE;
+
+-- Which departments have the highest number of employees?
+SELECT JD.JOBDEPT,COUNT(E.EMP_ID) AS NUMBER_OF_EMPLOYEES FROM JOBDEPARTMENT JD
+JOIN EMPLOYEE E ON JD.JOB_ID = E.JOB_ID
+GROUP BY JD.JOBDEPT
+ORDER BY NUMBER_OF_EMPLOYEES DESC;
+
+-- What is the average salary per department?
+SELECT JD.JOBDEPT,AVG(SB.AMOUNT) AS AVG_SALARY_PERDEPT 
+FROM JOBDEPARTMENT JD LEFT JOIN SALARYBONUS SB ON
+JD.JOB_ID = SB.JOB_ID
+GROUP BY JD.JOBDEPT;
+
+-- Who are the top 5 highest-paid employees?
+SELECT E.EMP_ID,E.FIRSTNAME,E.LASTNAME ,SB.AMOUNT FROM
+EMPLOYEE E JOIN SALARYBONUS SB ON
+E.EMP_ID = SB.JOB_ID
+ORDER BY SB.AMOUNT DESC
+LIMIT 5;
+
+-- What is the total salary expenditure across the company?
+SELECT SUM(SB.AMOUNT) AS SALARY_EXPENDITURE
+FROM EMPLOYEE E JOIN SALARYBONUS SB
+ON E.JOB_ID = SB.JOB_ID;
+
+-- 2. JOB ROLE AND DEPARTMENT ANALYSIS
+-- How many different job roles exist in each department?
+SELECT JOBDEPT,COUNT(JOB_ID) AS COUNT_OF_JOBROLES
+FROM JOBDEPARTMENT
+GROUP BY JOBDEPT
+ORDER BY  COUNT_OF_JOBROLES DESC;
+
+-- What is the average salary range per department?
+SELECT JD.JOBDEPT, MAX(SB.AMOUNT)-MIN(SB.AMOUNT) AS AVG_SALARYRANGE
+FROM JOBDEPARTMENT JD JOIN SALARYBONUS SB ON
+JD.JOB_ID = SB.JOB_ID
+GROUP BY JD.JOBDEPT;
+
+-- Which job roles offer the highest salary?
+SELECT JD.NAME,JD.JOBDEPT,MAX(SB.AMOUNT) AS HIGHEST_SALARY 
+FROM JOBDEPARTMENT JD JOIN SALARYBONUS SB ON
+JD.JOB_ID = SB.JOB_ID
+GROUP BY JD.NAME,JD.JOBDEPT
+ORDER BY HIGHEST_SALARY  DESC ;
+
+-- Which departments have the highest total salary allocation?
+SELECT JD.JOBDEPT,SUM(SB.AMOUNT) AS HIGHEST_TOTALSALARY
+FROM JOBDEPARTMENT JD JOIN SALARYBONUS SB ON
+JD.JOB_ID = SB.JOB_ID
+GROUP BY JD.JOBDEPT
+ORDER BY HIGHEST_TOTALSALARY DESC;
+
+--  3.QUALIFICATION AND SKILLS ANALYSIS
+-- How many employees have at least one qualification listed?
+SELECT COUNT(DISTINCT E.EMP_ID) AS EMPLOYEES_QUALIFIACTION 
+FROM EMPLOYEE E JOIN QUALIFICATION Q 
+ON E.EMP_ID = Q.EMP_ID;
+
+-- Which positions require the most qualifications?
+SELECT POSITION, COUNT(*) AS MOST_QUALIFIACTION
+FROM QUALIFICATION 
+GROUP BY POSITION
+ORDER BY MOST_QUALIFIACTION DESC;
+
+--  Which employees have the highest number of qualifications?
+SELECT E.EMP_ID,E.FIRSTNAME,E.LASTNAME,COUNT(Q.QUALID) AS HIGHEST_QUALIFICATIONS
+FROM EMPLOYEE E JOIN QUALIFICATION Q ON 
+E.EMP_ID = Q.EMP_ID
+GROUP BY E.EMP_ID,E.FIRSTNAME,E.LASTNAME
+ORDER BY HIGHEST_QUALIFICATIONS DESC;
+
+-- 4. LEAVE AND ABSENCE PATTERNS
+--  Which year had the most employees taking leaves?
+SELECT YEAR(DATE),COUNT(DISTINCT LEAVE_ID) AS EMPLOYEESLEAVES_IN_YEAR
+FROM LEAVES
+GROUP BY YEAR(DATE)
+ORDER BY EMPLOYEESLEAVES_IN_YEAR DESC;
+
+-- What is the average number of leave days taken by its employees per department?
+SELECT JD.JOBDEPT,AVG(EMP_LEAVES.LEAVE_COUNT) AS AVG_LEAVES
+FROM JOBDEPARTMENT JD
+JOIN EMPLOYEE E ON JD.JOB_ID = E.JOB_ID
+LEFT JOIN (
+    SELECT EMP_ID, COUNT(*) AS LEAVE_COUNT
+    FROM LEAVES
+    GROUP BY EMP_ID
+) EMP_LEAVES ON E.EMP_ID = EMP_LEAVES.EMP_ID
+GROUP BY JD.JOBDEPT
+ORDER BY AVG_LEAVES DESC;
+
+-- Which employees have taken the most leaves?
+SELECT E.EMP_ID,E.FIRSTNAME,E.LASTNAME, COUNT(L.LEAVE_ID) AS MOST_LEAVES
+FROM EMPLOYEE E JOIN LEAVES L
+ON E.EMP_ID = L.EMP_ID
+GROUP BY E.EMP_ID,E.FIRSTNAME,E.LASTNAME
+ORDER BY MOST_LEAVES DESC;
+
+-- What is the total number of leave days taken company-wide?
+SELECT COUNT(*) AS TOTAL_LEAVE_DAYS
+FROM LEAVES;
+
+-- How do leave days correlate with payroll amounts?
+SELECT 
+    E.EMP_ID,
+    E.FIRSTNAME,
+    E.LASTNAME,
+    COUNT(L.LEAVE_ID) AS TOTAL_LEAVES,
+    SUM(P.TOTAL_AMOUNT) AS TOTAL_PAYROLL
+FROM EMPLOYEE E
+LEFT JOIN LEAVES L ON E.EMP_ID = L.EMP_ID
+JOIN PAYROLL P ON E.EMP_ID = P.EMP_ID
+GROUP BY E.EMP_ID, E.FIRSTNAME, E.LASTNAME
+ORDER BY TOTAL_LEAVES DESC;
+-- If payroll decreases when leaves increase, correlation is negative.
+
+-- If payroll is independent of leaves, correlation is close to 0.
+
+-- If payroll increases with more leaves (rare), correlation is positive.
+
+-- 5. PAYROLL AND COMPENSATION ANALYSIS
+-- What is the total monthly payroll processed?
+SELECT 
+    DATE_FORMAT(DATE, '%Y-%m') AS MONTH,  
+    SUM(TOTAL_AMOUNT) AS TOTAL_MONTHLY_PAYROLL
+FROM PAYROLL
+GROUP BY DATE_FORMAT(date, '%Y-%m')
+ORDER BY MONTH;
+
+-- What is the average bonus given per department?
+SELECT JD.JOBDEPT, AVG(SB.BONUS) AS AVG_BONUS
+FROM JOBDEPARTMENT JD JOIN SALARYBONUS SB
+ON JD.JOB_ID = SB.JOB_ID
+GROUP BY JD.JOBDEPT;
+
+--  Which department receives the highest total bonuses?
+SELECT JD.JOBDEPT, SUM(SB.BONUS) AS TOTAL_BONUS
+FROM JOBDEPARTMENT JD JOIN SALARYBONUS SB
+ON JD.JOB_ID = SB.JOB_ID
+GROUP BY JD.JOBDEPT
+ORDER BY TOTAL_BONUS DESC;
+
+-- What is the average value of total_amount after considering leave deductions?
+SELECT AVG(P.TOTAL_AMOUNT) AS AVERAGE_TOTAL_PAYROLL
+FROM PAYROLL P;
+
+
+
+
+
